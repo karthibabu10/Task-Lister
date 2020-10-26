c99b485fc1a0c6c25cc13fe63d2f2be64dd706ad
Abhiramianilkumar <73107802+Abhiramianilkumar@users.noreply.github.com>
2020-10-26 19:04:23 +0530

    Added input field and add button
    Added input field and add button

--- src/App.js
+++ src/App.js
@@ -3,4 +3,29 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+    render() {
+        return (
+          <div>
+            
+            <h1 className="app-title"> Task Lister</h1>
+            <div className="container">
+              Add a Task
+              <br></br>
+              <input
+                type="text"
+                className="input-text"
+                placeholder="Write a todo"
+                value={this.state.newItem}
+                onChange={(e) => this.updateInput(e.target.value)}
+              ></input>
+              <button
+                className="add-btn"
+                onClick={() => this.addItem(this.state.newItem)}
+                disabled={!this.state.newItem.length}
+              >
+                Add to do
+              </button>
+              </div>
+              </div>
+              )
 } 
